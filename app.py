727f150ccb08b5807c90826d8a1a5bce3a0e6fe2
Naniyakkanti12 <152405825+Naniyakkanti12@users.noreply.github.com>
2025-07-08 22:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from resume_parser import extract_text_from_pdf
+from jd_parser import extract_text_from_pdf as extract_jd
+from ai_matcher import match_resume_to_job
+from email_sender import send_email
 
+resume_text = extract_text_from_pdf("uploads/resume.pdf")
+jd_text = extract_jd("uploads/job_description.pdf")
+
+result = match_resume_to_job(resume_text, jd_text)
+
+print("\nAI Output:\n", result)
+
+shortlisted = "Yes" in result
+summary = result.split("4.")[1].strip()
+
+send_email(
+    to_email="candidate@example.com",
+    subject="Job Application Status",
+    message=("🎉 Shortlisted!\n\n" if shortlisted else "❌ Not Shortlisted.\n\n") + summary
+)
